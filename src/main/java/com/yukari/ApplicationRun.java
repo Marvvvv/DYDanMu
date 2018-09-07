28d3091607d4e97608a56d9e8b00913d9eb41d3d
maofeng <935677413@qq.com>
2018-09-07 17:01:07 +0800

    定时更换弹幕服务器

--- src/main/java/com/yukari/ApplicationRun.java
+++ src/main/java/com/yukari/ApplicationRun.java
@@ -2,29 +2,17 @@ package com.yukari;
 
 import com.yukari.client.DyBulletScreenClient;
 
-import com.yukari.entity.ServerInfo;
-import com.yukari.utils.ChangeServer;
 import com.yukari.utils.KeepAlive;
 import com.yukari.utils.KeepGetMsg;
-import com.yukari.utils.ServerUtil;
-import org.jsoup.Connection;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.List;
 import java.util.Properties;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-
 
 
 public class ApplicationRun {
-
 
     public static void main(String[] args) {
 
@@ -36,10 +24,11 @@ public class ApplicationRun {
         try {
             inputStream = ApplicationRun.class.getClassLoader().getResourceAsStream("prop.properties");
             properties.load(inputStream);
-            roomId = Integer.parseInt(properties.getProperty("room.id","196"));
+            roomId = Integer.parseInt(properties.getProperty("room.id", "196"));
 
             DyBulletScreenClient client = DyBulletScreenClient.getInstance();
             client.init(roomId, -9999);
+
 
             // 发送心跳包线程
             KeepAlive alive = new KeepAlive();
@@ -55,7 +44,7 @@ public class ApplicationRun {
 
         } catch (Exception e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             if (inputStream != null) {
                 try {
                     inputStream.close();
---
+++ src/main/java/com/yukari/utils/ChangeServerTask.java
@@ -0,0 +1,19 @@
+package com.yukari.utils;
+
+import com.yukari.client.DyBulletScreenClient;
+import org.apache.log4j.Logger;
+
+
+public class ChangeServerTask {
+
+    Logger logger = Logger.getLogger(ChangeServerTask.class);
+
+    public void run() {
+        // 更换服务器重连
+        logger.info("更换弹幕服务器...");
+        DyBulletScreenClient client = DyBulletScreenClient.getInstance();
+        client.setReadyFlag(false);
+        client.init(196, -9999);
+    }
+
+}
